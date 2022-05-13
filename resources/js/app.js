f674427bcefa19ef8fb0e3f43ff475e8315903f1
Karim <karim@gojabu.com>
2022-05-14 00:45:16 +0200

    frontend

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,12 @@
-require('./bootstrap');
+import React from 'react'
+import { render } from 'react-dom'
+import { InertiaProgress } from '@inertiajs/progress'
+import { createInertiaApp } from '@inertiajs/inertia-react'
+
+InertiaProgress.init()
+createInertiaApp({
+    resolve: name => import(`./Pages/${name}`),
+    setup({ el, App, props }) {
+        render(<App {...props} />, el)
+    },
+})
--- webpack.mix.js
+++ webpack.mix.js
@@ -10,8 +10,10 @@ const mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
-
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+mix.webpackConfig({
+    output: {
+        chunkFilename: 'js/[name].js?id=[chunkhash]',
+    }
+});
+mix.js('resources/js/app.js', 'public/js').react()
+    .sass('resources/css/app.scss', 'public/css');
